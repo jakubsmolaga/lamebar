dc51ca10ab74ece9a9086975138137ef50ffbd1f
Jakub Smolaga <smolagakuba@gmail.com>
2025-10-24 18:09:29 +0200

    feat: ipc through named pipes

--- src/lamebar/ipc.c
+++ src/lamebar/ipc.c
@@ -1,11 +1,59 @@
 #include "ipc.h"
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/file.h>
 
-void ipc_init(void) { printf("ipc_init\n"); }
-void ipc_send(IPC_Cmd cmd) { printf("ipc_send %d\n", cmd); }
+// TODO: replace with /run/user/$UID/...
+//       this will allow multiple users to run their own daemons
+static const char *ipc_pipe_path = "/tmp/lamebar.pipe";
+static int ipc_pipe_fd = -1;
+
+void ipc_init(void) {
+	unlink(ipc_pipe_path);
+	int ret = mkfifo(ipc_pipe_path, 0600);
+	if (ret < 0) {
+		perror("failed to create fifo");
+		exit(1);
+	}
+	ipc_pipe_fd = open(ipc_pipe_path, O_RDWR | O_CREAT, 0600);
+	if (ipc_pipe_fd < 0) {
+		perror("failed to open pipe");
+		exit(1);
+	}
+}
+
+void ipc_send(IPC_Cmd cmd) { 
+	int fd = open(ipc_pipe_path, O_WRONLY);
+	if (fd < 0) {
+		perror("failed to open pipe");
+		exit(1);
+	}
+	switch (cmd) {
+	case IPC_CMD_SHOW:
+		write(fd, "1", 1);
+		break;
+	case IPC_CMD_HIDE:
+		write(fd, "0", 1);
+		break;
+	default:
+		fprintf(stderr, "unknown cmd %d\n", cmd);
+		exit(1);
+	}
+}
+
 IPC_Cmd ipc_recv(void) { 
-	printf("ipc_recv\n"); 
-	sleep(1);
-	return IPC_CMD_SHOW;
+	char c;
+	int ret = read(ipc_pipe_fd, &c, 1);
+	printf("ipc_recv %d\n", ret);
+	switch (c) {
+	case '1': return IPC_CMD_SHOW;
+	case '0': return IPC_CMD_HIDE;
+	default: return IPC_CMD_NONE;
+	}
 }
