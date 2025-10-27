08797fd1872ccd679bc08cf04aa07248a4ca0e34
Jakub Smolaga <smolagakuba@gmail.com>
2025-10-27 08:27:19 +0100

    refactor(wayland): use bufsock

--- src/lamebar/wl.c
+++ src/lamebar/wl.c
@@ -10,14 +10,13 @@
 #include <sys/poll.h>
 #include <unistd.h>
 #include <assert.h>
-#include "../base/arena.h"
 #include "../base/log.h"
+#include "bufsock.h"
 
 static struct {
 	int fd;
 	u32 next_id;
-	Arena rcv_arena, snd_arena;
-	void *rcv_pos;
+	BufSock bs;
 	int shm_fd;
 	PixelBuf fb;
 	u32 scale;
@@ -42,102 +41,35 @@ wl_connect(void)
 	wl.fd = socket(AF_UNIX, SOCK_STREAM, 0);
 	int ret = connect(wl.fd, (void*)&addr, sizeof(addr));
 	log_assert(ret >= 0, "failed to connect to wayland socket");
-}
-
-static void
-wl_fill_rcvbuf(void)
-{
-	arena_clear(&wl.rcv_arena);
-	wl.rcv_pos = arena_pos(&wl.rcv_arena);
-	i64 len = recv(wl.fd, wl.rcv_pos, ARENA_SIZE, 0);
-	arena_push_raw(&wl.rcv_arena, len, 1);
-}
-
-static void*
-wl_recv(u64 len)
-{
-	if (len == 0) return 0;
-	if (wl.rcv_pos == arena_pos(&wl.rcv_arena)) wl_fill_rcvbuf();
-	void *ptr = wl.rcv_pos;
-	wl.rcv_pos += len;
-	return ptr;
-}
-
-static u32 wl_recv_u32(void) { return *(u32*)wl_recv(sizeof(u32)); }
-static WL_Hdr wl_recv_hdr(void) { return *(WL_Hdr*)wl_recv(sizeof(WL_Hdr)); }
-static char* wl_recv_str(void) { u32 len = wl_recv_u32(); return wl_recv(align_up(len, 4)); }
-static bool wl_has_data(void) { 
-	if (wl.rcv_pos != arena_pos(&wl.rcv_arena))
-		return true;
-	struct pollfd pfd = { .fd = wl.fd, .events = POLLIN };
-	int ret = poll(&pfd, 1, 0);
-	return (ret > 0) && (pfd.revents & POLLIN);
+	wl.bs = bufsock_create(wl.fd);
 }
 
 static WL_Hdr*
 wl_msg_begin(u32 obj, u16 opcode) {
-	WL_Hdr *hdr = arena_push(&wl.snd_arena, WL_Hdr);
-	hdr->obj = obj;
-	hdr->opcode = opcode;
-	hdr->size = sizeof(WL_Hdr);
-	return hdr;
+	WL_Hdr hdr = { .obj = obj, .opcode = opcode, .size = sizeof(WL_Hdr) };
+	return bufsock_put(&wl.bs, &hdr, sizeof(WL_Hdr));
 }
 
 static void*
 wl_msg_push(WL_Hdr *hdr, const void *data, u64 len)
 {
-	void *ptr = arena_push_raw(&wl.snd_arena, len, 1);
-	memcpy(ptr, data, len);
+	void *ptr = bufsock_put(&wl.bs, data, len);
 	hdr->size += len;
 	return ptr;
 }
 
-static void wl_msg_push_u32(WL_Hdr *hdr, u32 data) { wl_msg_push(hdr, &data, sizeof(u32)); }
-static void wl_msg_push_str(WL_Hdr *hdr, const char *str) {
+static void
+wl_msg_push_u32(WL_Hdr *hdr, u32 data)
+{
+	wl_msg_push(hdr, &data, sizeof(u32));
+}
+
+static void
+wl_msg_push_str(WL_Hdr *hdr, const char *str)
+{
 	u64 len = strlen(str) + 1;
 	wl_msg_push_u32(hdr, len);
 	wl_msg_push(hdr, str, align_up(len, 4));
-}
-
-static void
-wl_flush(void)
-{
-	void *base = wl.snd_arena.base;
-	u64 size = wl.snd_arena.pos - base;
-	if (size > 0) send(wl.fd, base, size, 0);
-	arena_clear(&wl.snd_arena);
-}
-
-static void
-wl_flush_with_fd(int extra_fd)
-{
-	void *base = wl.snd_arena.base;
-	u64 size = wl.snd_arena.pos - base;
-	log_assert(size > 0, "wl_flush_with_fd() called with empty buffer");
-	struct iovec iov = {
-		.iov_base = base,
-		.iov_len  = size,
-	};
-
-	u8 control[CMSG_SPACE(sizeof(extra_fd))];
-	memset(control, 0, sizeof(control));
-
-	struct msghdr msgh = {0};
-	msgh.msg_iov = &iov;
-	msgh.msg_iovlen = 1;
-	msgh.msg_control = control;
-	msgh.msg_controllen = sizeof(control);
-
-	struct cmsghdr *cmsg = CMSG_FIRSTHDR(&msgh);
-	cmsg->cmsg_len   = CMSG_LEN(sizeof(extra_fd));
-	cmsg->cmsg_level = SOL_SOCKET;
-	cmsg->cmsg_type  = SCM_RIGHTS;
-
-	// Copy the fd into the ancillary data
-	*((int*) CMSG_DATA(cmsg)) = extra_fd;
-
-	sendmsg(wl.fd, &msgh, 0);
-	arena_clear(&wl.snd_arena);
 }
 
 static void*
@@ -194,15 +126,16 @@ wl_grow_shared_memory(int fd, void *ptr, u64 old_size, u64 new_size)
 static void
 wl_drain_events(void)
 {
-	while (wl_has_data()) {
-		WL_Hdr hdr = wl_recv_hdr();
+	while (bufsock_has_data(&wl.bs)) {
+		WL_Hdr hdr = *(WL_Hdr*)bufsock_get(&wl.bs, sizeof(WL_Hdr));
 		if (hdr.obj != 1 || hdr.opcode != 0) {
-			wl_recv(hdr.size - sizeof(hdr));
+			bufsock_get(&wl.bs, hdr.size - sizeof(hdr));
 			continue;
 		}
-		u32 obj_id = wl_recv_u32();
-		u32 errcode = wl_recv_u32();
-		char *msg = wl_recv_str();
+		u32 obj_id = *(u32*)bufsock_get(&wl.bs, sizeof(u32));
+		u32 errcode = *(u32*)bufsock_get(&wl.bs, sizeof(u32));
+		u32 msg_len = *(u32*)bufsock_get(&wl.bs, sizeof(u32));
+		char *msg = bufsock_get(&wl.bs, align_up(msg_len, 4));
 		log_crash("wayland error (%d) (obj=%d): %s", errcode, obj_id, msg);
 	}
 }
@@ -289,7 +222,7 @@ wl_shm_create_pool(u32 self, int mem_fd, u32 size)
 	WL_Hdr *hdr = wl_msg_begin(self, 0);
 	wl_msg_push_u32(hdr, id);
 	wl_msg_push_u32(hdr, size);
-	wl_flush_with_fd(mem_fd);
+	bufsock_flush_with_fd(&wl.bs, mem_fd);
 	return id;
 }
 
@@ -355,16 +288,17 @@ wl_surface_damage_buffer(u32 self, u32 x, u32 y, u32 w, u32 h)
 static void
 wl_bind_interfaces(u32 registry, u32 *compositor, u32 *layer_shell, u32 *shm)
 {
-	wl_fill_rcvbuf();
-	while (wl_has_data()) {
-		WL_Hdr hdr = wl_recv_hdr();
+	bufsock_fill(&wl.bs);
+	while (bufsock_has_data(&wl.bs)) {
+		WL_Hdr hdr = *(WL_Hdr*)bufsock_get(&wl.bs, sizeof(WL_Hdr));
 		if (hdr.obj != registry || hdr.opcode != 0) {
-			wl_recv(hdr.size - sizeof(hdr)); // skip
+			bufsock_get(&wl.bs, hdr.size - sizeof(hdr)); // skip
 			continue;
 		}
-		u32 name = wl_recv_u32();
-		char *iface = wl_recv_str();
-		u32 version = wl_recv_u32();
+		u32 name = *(u32*)bufsock_get(&wl.bs, sizeof(u32));
+		u32 iface_len = *(u32*)bufsock_get(&wl.bs, sizeof(u32));
+		char *iface = bufsock_get(&wl.bs, align_up(iface_len, 4));
+		u32 version = *(u32*)bufsock_get(&wl.bs, sizeof(u32));
 		if (strcmp(iface, "wl_compositor") == 0)
 			*compositor = wl_registry_bind(registry, name, iface, version);
 		else if (strcmp(iface, "zwlr_layer_shell_v1") == 0)
@@ -381,14 +315,14 @@ static u32
 wl_wait_for_configure(u32 layer_surface)
 {
 	while (true) {
-		WL_Hdr hdr = wl_recv_hdr();
+		WL_Hdr hdr = *(WL_Hdr*)bufsock_get(&wl.bs, sizeof(WL_Hdr));
 		if (hdr.obj == layer_surface && hdr.opcode == 0) {
-			u32 serial = wl_recv_u32();
-			wl_recv_u32(); // width
-			wl_recv_u32(); // height
+			u32 serial = *(u32*)bufsock_get(&wl.bs, sizeof(u32));
+			bufsock_get(&wl.bs, sizeof(u32)); // width
+			bufsock_get(&wl.bs, sizeof(u32)); // height
 			return serial;
 		}
-		wl_recv(hdr.size - sizeof(hdr)); // skip
+		bufsock_get(&wl.bs, hdr.size - sizeof(hdr)); // skip
 	}
 }
 
@@ -411,7 +345,7 @@ wl_ensure_fb_size(u32 w, u32 h)
 	zwlr_layer_surface_v1_set_size(wl.layer_surface, w, h);
 	zwlr_layer_surface_v1_set_anchor(wl.layer_surface, WL_ANCHOR_TOP | WL_ANCHOR_RIGHT);
 	wl_surface_commit(wl.surface);
-	wl_flush();
+	bufsock_flush(&wl.bs);
 	u32 serial = wl_wait_for_configure(wl.layer_surface);
 	zwlr_layer_surface_v1_ack_configure(wl.layer_surface, serial);
 }
@@ -421,14 +355,11 @@ wl_ensure_fb_size(u32 w, u32 h)
 void wl_init(u32 scale) {
 	wl.scale = scale;
 	wl.next_id = 2; // 0=NULL, 1=display
-	wl.rcv_arena = arena_create();
-	wl.snd_arena = arena_create();
-	wl.rcv_pos = arena_pos(&wl.rcv_arena);
 
 	wl_connect();
 
 	u32 registry = wl_display_get_registry();
-	wl_flush();
+	bufsock_flush(&wl.bs);
 	u32 compositor = 0, layer_shell = 0;
 	wl_bind_interfaces(registry, &compositor, &layer_shell, &wl.shm);
 
@@ -440,7 +371,7 @@ void wl_init(u32 scale) {
 	zwlr_layer_surface_v1_set_size(wl.layer_surface, width * scale, height * scale);
 	zwlr_layer_surface_v1_set_anchor(wl.layer_surface, WL_ANCHOR_TOP | WL_ANCHOR_RIGHT);
 	wl_surface_commit(wl.surface);
-	wl_flush();
+	bufsock_flush(&wl.bs);
 	u32 serial = wl_wait_for_configure(wl.layer_surface);
 	zwlr_layer_surface_v1_ack_configure(wl.layer_surface, serial);
 
@@ -455,7 +386,7 @@ void wl_init(u32 scale) {
 	wl_surface_attach(wl.surface, wl.buffer, 0, 0);
 	wl_surface_damage_buffer(wl.surface, 0, 0, wl.fb.w, wl.fb.h);
 	wl_surface_commit(wl.surface);
-	wl_flush();
+	bufsock_flush(&wl.bs);
 	wl_drain_events();
 }
 
@@ -464,7 +395,7 @@ void wl_hide(void) {
 	wl_surface_attach(wl.surface, wl.buffer, 0, 0);
 	wl_surface_damage_buffer(wl.surface, 0, 0, wl.fb.w, wl.fb.h);
 	wl_surface_commit(wl.surface);
-	wl_flush();
+	bufsock_flush(&wl.bs);
 }
 
 void
@@ -472,12 +403,12 @@ wl_show(PixelBuf pixels)
 {
 	wl_ensure_fb_size(pixels.w * wl.scale, pixels.h * wl.scale);
 	log_assert(pixels.w <= wl.fb.w && pixels.h <= wl.fb.h, "frame is too large for the framebuffer");
-	wl_flush();
+	bufsock_flush(&wl.bs);
 	wl_drain_events();
 	memset(wl.fb.data, 0, wl.fb.w * wl.fb.h * sizeof(Pixel));
 	pixelbuf_copy(wl.fb, pixels, wl.fb.w - pixels.w * wl.scale, 0, wl.scale);
 	wl_surface_attach(wl.surface, wl.buffer, 0, 0);
 	wl_surface_damage_buffer(wl.surface, 0, 0, wl.fb.w, wl.fb.h);
 	wl_surface_commit(wl.surface);
-	wl_flush();
+	bufsock_flush(&wl.bs);
 }
